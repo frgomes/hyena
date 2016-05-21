c02a93578cfb9227ee11e8ea8fa01dbf114f1c34
Richard Gomes <rgomes.info@gmail.com>
2016-05-21 22:52:18 +0100

    Define commands as functions, instead of aliases, since functions are more flexible, being listed in Bash and passed onto sub-shells.

--- bash_xkb.sh
+++ bash_xkb.sh
@@ -1,59 +1,57 @@
 #!/bin/bash
 
-[[ $_ == $0 ]] && echo "ERROR: This script must be sourced"
+[[ $_ == $0 ]] && echo "ERROR: This script must be sourced" && exit 1
 
-alias       kb_us='setxkbmap -v 9 -model pc104 -layout "us" -option ""'
-alias       kb_gb='setxkbmap -v 9 -model pc104 -layout "gb" -option ""'
+# standard PC keyboard with standard US and GB layouts, regular and international
+function carpalx_pc_us           { setxkbmap -v 9 -model pc104 -layout "us" -option ""; }
+function carpalx_pc_gb           { setxkbmap -v 9 -model pc104 -layout "gb" -option ""; }
+function carpalx_pc_usintl       { setxkbmap -v 9 -model pc104 -layout "us(intl)" -option ""; }
+function carpalx_pc_gbintl       { setxkbmap -v 9 -model pc104 -layout "gb(intl)" -option ""; }
 
-alias       kb_usintl='setxkbmap -v 9 -model pc104 -layout "us(intl)" -option ""'
-alias       kb_gbintl='setxkbmap -v 9 -model pc104 -layout "gb(intl)" -option ""'
+# standard PC keyboard with various alternative CarpalX layouts, swap Ctrl with Alt
+function carpalx_pc_qgmlwb       { setxkbmap -v 9 -model pc104 -layout "carpalx(qgmlwb)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
+function carpalx_pc_qgmlwy       { setxkbmap -v 9 -model pc104 -layout "carpalx(qgmlwy)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
+function carpalx_pc_qfmlwy       { setxkbmap -v 9 -model pc104 -layout "carpalx(qfmlwy)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
+function carpalx_pc_qwrkfy       { setxkbmap -v 9 -model pc104 -layout "carpalx(qwrkfy)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
+function carpalx_pc_qwyrfm       { setxkbmap -v 9 -model pc104 -layout "carpalx(qwyrfm)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
+function carpalx_pc_tnwmlc       { setxkbmap -v 9 -model pc104 -layout "carpalx(tnwmlc)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
 
-alias      kbt_us='setxkbmap -v 9 -model pc104 -layout "yoda(us)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
-alias      kbt_gb='setxkbmap -v 9 -model pc104 -layout "yoda(gb)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
-alias  kbt_usintl='setxkbmap -v 9 -model pc104 -layout "yoda(us-intl)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
-alias  kbt_gbintl='setxkbmap -v 9 -model pc104 -layout "yoda(gb-intl)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
 
-alias      kbm_us='setxkbmap -v 9 -model pc104 -layout "miniguru(us)+carpalx(swap_lalt_lctl)"'
-alias      kbm_gb='setxkbmap -v 9 -model pc104 -layout "miniguru(gb)+carpalx(swap_lalt_lctl)"'
-alias  kbm_usintl='setxkbmap -v 9 -model pc104 -layout "miniguru(us-intl)+carpalx(swap_lalt_lctl)"'
-alias  kbm_gbintl='setxkbmap -v 9 -model pc104 -layout "miniguru(gb-intl)+carpalx(swap_lalt_lctl)"'
 
-alias  kbc_qgmlwb='setxkbmap -v 9 -model pc104 -layout "carpalx(qgmlwb)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
-alias  kbc_qgmlwy='setxkbmap -v 9 -model pc104 -layout "carpalx(qgmlwy)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
-alias  kbc_qfmlwy='setxkbmap -v 9 -model pc104 -layout "carpalx(qfmlwy)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
-alias  kbc_qwrkfy='setxkbmap -v 9 -model pc104 -layout "carpalx(qwrkfy)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
-alias  kbc_qwyrfm='setxkbmap -v 9 -model pc104 -layout "carpalx(qwyrfm)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
-alias  kbc_tnwmlc='setxkbmap -v 9 -model pc104 -layout "carpalx(tnwmlc)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
+# TEX Yoda keyboard with standard US and GB layouts, regular and international, swap Ctrl with Alt
+function carplax_tex_us          { setxkbmap -v 9 -model pc104 -layout "yoda(us)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
+function carplax_tex_gb          { setxkbmap -v 9 -model pc104 -layout "yoda(gb)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
+function carplax_tex_usintl      { setxkbmap -v 9 -model pc104 -layout "yoda(us-intl)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
+function carplax_tex_gbintl      { setxkbmap -v 9 -model pc104 -layout "yoda(gb-intl)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
 
-alias kbct_qgmlwb='setxkbmap -v 9 -model pc104 -layout "carpalx(qgmlwb-yoda)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
-alias kbct_qgmlwy='setxkbmap -v 9 -model pc104 -layout "carpalx(qgmlwy-yoda)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
-alias kbct_qfmlwy='setxkbmap -v 9 -model pc104 -layout "carpalx(qfmlwy-yoda)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
-alias kbct_qwrkfy='setxkbmap -v 9 -model pc104 -layout "carpalx(qwrkfy-yoda)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
-alias kbct_qwyrfm='setxkbmap -v 9 -model pc104 -layout "carpalx(qwyrfm-yoda)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
-alias kbct_tnwmlc='setxkbmap -v 9 -model pc104 -layout "carpalx(tnwmlc-yoda)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
+# TEX Yoda keyboard with various alternative CarpalX layouts, swap Ctrl with Alt
+function carpalx_tex_qgmlwb      { setxkbmap -v 9 -model pc104 -layout "carpalx(qgmlwb-yoda)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
+function carpalx_tex_qgmlwy      { setxkbmap -v 9 -model pc104 -layout "carpalx(qgmlwy-yoda)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
+function carpalx_tex_qfmlwy      { setxkbmap -v 9 -model pc104 -layout "carpalx(qfmlwy-yoda)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
+function carpalx_tex_qwrkfy      { setxkbmap -v 9 -model pc104 -layout "carpalx(qwrkfy-yoda)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
+function carpalx_tex_qwyrfm      { setxkbmap -v 9 -model pc104 -layout "carpalx(qwyrfm-yoda)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
+function carpalx_tex_tnwmlc      { setxkbmap -v 9 -model pc104 -layout "carpalx(tnwmlc-yoda)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
 
-alias kbcm_qgmlwb='setxkbmap -v 9 -model pc104 -layout "carpalx(qgmlwb-miniguru)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
-alias kbcm_qgmlwy='setxkbmap -v 9 -model pc104 -layout "carpalx(qgmlwy-miniguru)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
-alias kbcm_qfmlwy='setxkbmap -v 9 -model pc104 -layout "carpalx(qfmlwy-miniguru)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
-alias kbcm_qwrkfy='setxkbmap -v 9 -model pc104 -layout "carpalx(qwrkfy-miniguru)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
-alias kbcm_qwyrfm='setxkbmap -v 9 -model pc104 -layout "carpalx(qwyrfm-miniguru)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
-alias kbcm_tnwmlc='setxkbmap -v 9 -model pc104 -layout "carpalx(tnwmlc-miniguru)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
 
-alias hyena_us='setxkbmap -v 9 -model pc104 -layout "hyena(us)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
-alias hyena_gb='setxkbmap -v 9 -model pc104 -layout "hyena(gb)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
-alias hyena_usintl='setxkbmap -v 9 -model pc104 -layout "hyena(us-intl)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
-alias hyena_gbintl='setxkbmap -v 9 -model pc104 -layout "hyena(gb-intl)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"'
 
-alias kbus=kb_us
-alias kbgb=kb_gb
-alias kbtus=kbt_us
-alias kbtgb=kbt_gb
-alias kbmus=kbm_us
-alias kbmgb=kbm_gb
+# MiniGuru keyboard with standard US and GB layouts, regular and international, swap Ctrl with Alt
+function carpalx_miniguru_us     { setxkbmap -v 9 -model pc104 -layout "miniguru(us)+carpalx(swap_lalt_lctl)"; }
+function carpalx_miniguru_gb     { setxkbmap -v 9 -model pc104 -layout "miniguru(gb)+carpalx(swap_lalt_lctl)"; }
+function carpalx_miniguru_usintl { setxkbmap -v 9 -model pc104 -layout "miniguru(us-intl)+carpalx(swap_lalt_lctl)"; }
+function carpalx_miniguru_gbintl { setxkbmap -v 9 -model pc104 -layout "miniguru(gb-intl)+carpalx(swap_lalt_lctl)"; }
 
-alias kbusintl=kb_usintl
-alias kbgbintl=kb_gbintl
-alias kbtusintl=kbt_usintl
-alias kbtgbintl=kbt_gbintl
-alias kbmusintl=kbm_usintl
-alias kbmgbintl=kbm_gbintl
+# MiniGuru keyboard with various alternative CarpalX layouts, swap Ctrl with Alt
+function carpalx_miniguru_qgmlwb { setxkbmap -v 9 -model pc104 -layout "carpalx(qgmlwb-miniguru)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
+function carpalx_miniguru_qgmlwy { setxkbmap -v 9 -model pc104 -layout "carpalx(qgmlwy-miniguru)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
+function carpalx_miniguru_qfmlwy { setxkbmap -v 9 -model pc104 -layout "carpalx(qfmlwy-miniguru)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
+function carpalx_miniguru_qwrkfy { setxkbmap -v 9 -model pc104 -layout "carpalx(qwrkfy-miniguru)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
+function carpalx_miniguru_qwyrfm { setxkbmap -v 9 -model pc104 -layout "carpalx(qwyrfm-miniguru)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
+function carpalx_miniguru_tnwmlc { setxkbmap -v 9 -model pc104 -layout "carpalx(tnwmlc-miniguru)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
+
+
+
+# (Emacs friendly) Hyena keyboard with standard US and GB layouts, regular and international, swap Ctrl with Alt
+function carpalx_hyena_us        { setxkbmap -v 9 -model pc104 -layout "hyena(us)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
+function carpalx_hyena_gb        { setxkbmap -v 9 -model pc104 -layout "hyena(gb)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
+function carpalx_hyena_usintl    { setxkbmap -v 9 -model pc104 -layout "hyena(us-intl)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
+function carpalx_hyena_gbintl    { setxkbmap -v 9 -model pc104 -layout "hyena(gb-intl)+carpalx(spce_bksp)+carpalx(swap_lalt_lctl)"; }
